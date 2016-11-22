6b142604ab71e15fb2c69dfb710eb081d1768e6e
rudneff <dmitryrudneff@gmail.com>
2016-11-22 18:04:25 +0300

    Server done.

--- httpd.py
+++ httpd.py
@@ -1,24 +1,39 @@
+import argparse
+import configparser
+import os
 import socket
+
+import sys
 
 from core.settings import DEFAULT_ROOT_DIR
 from src.handler import CustomHttpRequestHandler
-from src.request import Request
+from src.server import CustomHttpServer
+
+DEFAULT_CONFIG = 'config.ini'
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-C', '--config', default=DEFAULT_CONFIG, help='Configuration file')
+    parser.add_argument(
+        '-r', '--root', default=DEFAULT_ROOT_DIR, help='Root directory for reading files')
+    parser.add_argument(
+        '-c', '--ncpu', default=3, type=int, help='Number of cpu')
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('127.0.0.1', 8080))
-    sock.listen(10)
-    while True:
-        client_sock, client_addr = sock.accept()
-        data = client_sock.recv(2048)
-        if len(data.strip()) == 0:
-            client_sock.close()
-            continue
-        req = Request(data)
-        handler = CustomHttpRequestHandler(req, DEFAULT_ROOT_DIR)
-        resp = handler.handle()
-        client_sock.sendall(resp.build())
-        client_sock.close()
-    sock.close()
+    args = parse_arguments()
+    config = configparser.RawConfigParser()
+    config.read(args.config)
 
+    if not os.path.exists(args.root):
+        print("Please, check root directory.")
+        sys.exit()
+
+    server = CustomHttpServer(args.root, CustomHttpRequestHandler, config.get('server', 'host'),
+                              int(config.get('server', 'port')), args.ncpu, int(config.get('server', 'listeners')),
+                              int(config.get('server', 'recv_msg_size')))
+    server.start()
